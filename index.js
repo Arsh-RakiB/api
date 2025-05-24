e7688c06a15fcd27cb263f8a0197b3be3a799f58
RakiB <mdrakib01730653037@gmail.com>
2025-05-24 18:27:17 +0600

    final

---
+++ index.js
@@ -0,0 +1,11 @@
+import express from "express";
+import authrouter from "./router/router.js";
+import DBconnect from "./db/db_connection.js";
+const app=express();
+// const port = 3000;
+app.use(express.json())
+app.use('/api/user_register', authrouter)
+// app.listen(port, () => {
+//     console.log(`Server started on port ${port}`);
+// });
+DBconnect()
